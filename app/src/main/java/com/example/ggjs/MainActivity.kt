9c7936631bbd11a638075e0ffc4f159887f1cfb1
lidegui <1012610779@qq.com>
2025-10-04 00:08:13 +0800

    测试Java层调用js代码

--- app/src/main/java/com/example/ggjs/MainActivity.kt
+++ app/src/main/java/com/example/ggjs/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.ggjs
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,9 +10,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.ggjs.ui.theme.GGJSTheme
+import com.example.quickjs.JSContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +26,7 @@ class MainActivity : ComponentActivity() {
             GGJSTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
+                        this,
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -31,9 +37,16 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(context: Context, name: String, modifier: Modifier = Modifier) {
+    val jsContext = remember { mutableStateOf(JSContext(context)) }
+    val evalCode = jsContext.value.jsRuntime.evalCode(
+        "print(\"开始执行一段js代码并且返回一个int值\");\n" +
+                "a=1;\n" +
+                "b=2;\n" +
+                "a+b;", "GGJS"
+    )
     Text(
-        text = "Hello $name!",
+        text = "Hello $name!，invoke js and result is $evalCode",
         modifier = modifier
     )
 }
@@ -42,6 +55,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     GGJSTheme {
-        Greeting("Android")
+        Greeting(LocalContext.current, "Android")
     }
 }
--- quickJS/src/main/java/com/example/quickjs/NativeLib.kt
+++ quickJS/src/main/java/com/example/quickjs/NativeLib.kt
@@ -20,13 +20,13 @@ class NativeLib {
         // Used to load the 'quickjs' library on application startup.
         init {
             System.loadLibrary("quickjs")
-            val nativeLib = NativeLib()
-            val stringFromJNI = nativeLib.stringFromJNI()
-            Log.i("ldg-test", ": stringFromJNI = $stringFromJNI")
-            val testQuickJS = nativeLib.testQuickJS()
-            Log.i("ldg-test", ": testQuickJS = $testQuickJS")
-            nativeLib.testJsInvokeC()
-            nativeLib.testJSProxyObject()
+//            val nativeLib = NativeLib()
+//            val stringFromJNI = nativeLib.stringFromJNI()
+//            Log.i("ldg-test", ": stringFromJNI = $stringFromJNI")
+//            val testQuickJS = nativeLib.testQuickJS()
+//            Log.i("ldg-test", ": testQuickJS = $testQuickJS")
+//            nativeLib.testJsInvokeC()
+//            nativeLib.testJSProxyObject()
         }
     }
 }
